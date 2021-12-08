5e120c977b5255a6fdde1f7ea0116ee113404558
alon <alon359@gmail.com>
2021-12-08 20:35:17 +0200

    up

---
+++ Front/sports-results/src/app/services/print-service.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrintServiceService {
+
+  constructor() { }
+
+  GalitSex(): string {
+    return 'I want sex with galit when she smoke'
+    
+  }
+}
