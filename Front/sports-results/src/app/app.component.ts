64a00c0ba3502dd19e74db55a6f5c6da4ee7957d
alon <alon359@gmail.com>
2021-12-09 14:18:50 +0200

    up

--- Front/sports-results/src/app/app.component.ts
+++ Front/sports-results/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PrintServiceService } from './services/print-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +6,9 @@ import { PrintServiceService } from './services/print-service.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private printS: PrintServiceService){}
-
-  title = 'sports-results';
-
+  constructor(){}
   ngOnInit(): void{
-    console.log(this.printS.GalitSex());
-    
   }
+
+
 }
